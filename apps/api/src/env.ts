57a01ce90e464befefcd965ccf48303baad8202a
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-07-15 05:01:59 +0100

    chore: set up type-safe env

---
+++ apps/api/src/env.ts
@@ -0,0 +1,22 @@
+import z, { type ZodError } from "zod";
+
+const EnvSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  PORT: z.coerce.number().default(8000),
+});
+
+export type Env = z.infer<typeof EnvSchema>;
+
+let env: Env;
+
+try {
+  env = EnvSchema.parse(process.env);
+} catch (e) {
+  const error = e as ZodError;
+  console.error(z.prettifyError(error));
+  process.exit(1);
+}
+
+export default env;
