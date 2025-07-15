0e75fae99bc7f58d6db6533111fe74dce2bfc60f
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-07-15 05:02:14 +0100

    refactor: update api app

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,13 +1,27 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { logger } from "hono/logger";
 
-import { notFoundRoute } from "@/middleware/not-found-route";
+import emojiFavicon from "./middleware/emoji-favicon";
+import errorHandler from "./middleware/error-handler";
+import notFoundRoute from "./middleware/not-found-route";
 
-const app = new Hono();
+const app = new OpenAPIHono();
+
+// Middleware for Logging requests and setting up the emoji favicon
+app.use(logger());
+app.use(emojiFavicon("📔"));
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/error", (c) => {
+  c.status(422);
+  throw new Error("This is a test error");
+});
+
+// Middleware for handling errors and not found routes
 app.notFound(notFoundRoute);
+app.onError(errorHandler);
 
 export default app;
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
+import env from "./env";
 
-const port = process.env.PORT || 8000;
+const port = env.PORT;
 
 export default {
   port,
