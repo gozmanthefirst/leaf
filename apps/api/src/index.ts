3a0aba54e4342477f70f74b668422f3bee027361
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-08-19 01:23:40 +0100

    refactor: remove unnecessary index router

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,11 @@
 import createApp from "@/lib/create-app";
 import configureOpenAPI from "@/lib/openapi";
 import authRouter from "./routes/auth/auth.index";
-import indexRouter from "./routes/index.route";
 import notesRouter from "./routes/notes/notes.index";
 
 const app = createApp();
 
-const routers = [indexRouter, notesRouter, authRouter];
+const routers = [notesRouter, authRouter];
 
 routers.forEach((router) => {
   app.route("/api", router);
