9d97cd2f54d91c702801e1867aa7496ef31faeb5
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-10-03 03:11:48 +0100

    chore: install various UI components

---
+++ apps/web/src/components/ui/separator.tsx
@@ -0,0 +1,26 @@
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
+import type { ComponentProps } from "react";
+
+import { cn } from "@/lib/utils";
+
+const Separator = ({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: ComponentProps<typeof SeparatorPrimitive.Root>) => {
+  return (
+    <SeparatorPrimitive.Root
+      className={cn(
+        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
+        className,
+      )}
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      {...props}
+    />
+  );
+};
+
+export { Separator };
