2ac81bc0c307fd43817a0835e1439a61b0b6dfb8
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-11-23 01:06:25 +0100

    chore: stop using dotenv in prisma config file

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
-import { defineConfig, env } from "prisma/config";
+import env from "./src/lib/env";
 
 export default defineConfig({
-  schema: "prisma/schema",
+  schema: "prisma/schema/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: env.DATABASE_URL,
   },
 });
