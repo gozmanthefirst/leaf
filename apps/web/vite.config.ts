b6fbed81d78178914e5aa1b3e4b2f2abac3f3591
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-12-04 04:41:54 +0100

    fix: tanstack router prod error

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -13,7 +13,7 @@ export default defineConfig({
     tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
-    nitro({ preset: "bun" }),
+    nitroV2Plugin({ preset: "bun" }),
     viteReact(),
   ],
 });
